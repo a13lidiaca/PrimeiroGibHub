b92d4c4e45f288a7e0defec4e5101ecd79144cf5
dam2 - Cotón Antelo <a13lidiaca@a11equ15.sanclemente.local>
2015-10-07 12:45:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a13lidiaca/primeirogibhub/PrimeiroGibHub.java
@@ -0,0 +1,13 @@
+package com.example.a13lidiaca.primeirogibhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimeiroGibHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeiro_gib_hub);
+    }
+}
